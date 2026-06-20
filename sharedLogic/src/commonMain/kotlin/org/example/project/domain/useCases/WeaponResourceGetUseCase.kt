7173e5876b32c4cb02a43d49f296f46316727610
Takamasa-Fukase <ultrafukase@gmail.com>
2026-06-20 18:37:44 +0900

    IFにし忘れてたのを修正

--- sharedLogic/src/commonMain/kotlin/org/example/project/domain/useCases/WeaponResourceGetUseCase.kt
+++ sharedLogic/src/commonMain/kotlin/org/example/project/domain/useCases/WeaponResourceGetUseCase.kt
@@ -1,14 +1,14 @@
 package org.example.project.domain.useCases
 
-import org.example.project.data.repositories.WeaponRepository
 import org.example.project.domain.entities.Weapon
+import org.example.project.domain.repositoryInterfaces.WeaponRepositoryInterface
 
 interface WeaponResourceGetUseCaseInterface {
     suspend fun execute(id: Int): Weapon
 }
 
 class WeaponResourceGetUseCase(
-    private val weaponRepository: WeaponRepository
+    private val weaponRepository: WeaponRepositoryInterface
 ): WeaponResourceGetUseCaseInterface {
     override suspend fun execute(id: Int): Weapon {
         return weaponRepository.getById(id = id)
